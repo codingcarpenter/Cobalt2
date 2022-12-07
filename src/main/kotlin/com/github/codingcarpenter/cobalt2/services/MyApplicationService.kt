4b92b95221b8802bb6572879b9887f7028d3fcec
GitHub Action <action@github.com>
2022-12-07 18:32:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/codingcarpenter/cobalt2/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/codingcarpenter/cobalt2/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.codingcarpenter.cobalt2.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.codingcarpenter.cobalt2.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/codingcarpenter/cobalt2/services/MyProjectService.kt
+++ src/main/kotlin/com/github/codingcarpenter/cobalt2/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.codingcarpenter.cobalt2.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.codingcarpenter.cobalt2.MyBundle
 
 class MyProjectService(project: Project) {
 
